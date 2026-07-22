f54aee9a7cb20230bf5c69f79f5379bde98f7060
vaidybhensdadiya <bhensdadiyavaidy@gmail.com>
2026-07-22 11:31:40 +0530

    modification in index.html

--- backend/dealership/urls.py
+++ backend/dealership/urls.py
@@ -1,16 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import JsonResponse
-
-def api_root(request):
-    return JsonResponse({
-        "name": "AutoVault Car Dealership API",
-        "status": "online",
-        "version": "1.0.0"
-    })
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', api_root, name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('inventory.urls')),
 ]
